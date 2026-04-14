51ef0da15ed51b16813ff63aaa4eb05e469ddb80
George <george.p@escolar.ifrn.edu.br>
2026-04-14 15:46:26 -0300

    Create exer1.py

---
+++ POO/herança/exer1.py
@@ -0,0 +1,11 @@
+class Computador():
+    def __init__(self, processador :str, memoria: int)
+    self.processador = processador
+    self.memoria = memoria
+
+
+
+class Laptop(Computador):
+
+
+class Desktop(Computador):
